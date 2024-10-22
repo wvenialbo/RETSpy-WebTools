ddd1140bab3366db3672093a3febcf8ba4104e8e
wvenialbo <wvenialbo@gmail.com>
2024-10-21 22:18:12 -0300

    refactor: Move #getElement method to GuiElement class

--- src/gui.js
+++ src/gui.js
@@ -128,18 +128,6 @@ class GuiElement {
     }
   }
 
-  static #getElement(element) {
-    if (element instanceof Element) {
-      return element;
-    }
-    if (element instanceof GuiElement) {
-      return element.element;
-    }
-    throw new TypeError(
-      "`source` must contain only instances of Element or GuiElement",
-    );
-  }
-
   attachEventObserver(name, observer) {
     this.addEventListener(name, (event) => {
       observer.dispatchEvent(event.name, event.parameters);
@@ -162,6 +150,18 @@ class GuiElement {
         this.dispatchEvent(destination, event);
       });
     }
+  }
+
+  static #getElement(element) {
+    if (element instanceof Element) {
+      return element;
+    }
+    if (element instanceof GuiElement) {
+      return element.element;
+    }
+    throw new TypeError(
+      "`source` must contain only instances of Element or GuiElement",
+    );
   }
 
   hide() {
